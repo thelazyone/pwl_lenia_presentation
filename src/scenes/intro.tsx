4c125de73e9c5467388bf1a1d5ad454ef537315a
thelazyone <giacomo.pantalone@gmail.com>
2025-05-04 18:16:11 +0200

    improved intro, but not not pretty.

--- src/scenes/intro.tsx
+++ src/scenes/intro.tsx
@@ -5,8 +5,10 @@ import {Colors, textStyles} from './shared';
 export default makeScene2D(function* (view) {
   view.lineHeight(64);
   const title = createRef<Txt>();
-  const subtitle = createRef<Txt>();
-  const definition = createRef<Txt>();
+  const computational = createRef<Txt>();
+  const abstract = createRef<Txt>();
+  const discrete = createRef<Txt>();
+  const emergent = createRef<Txt>();
   const layout = createRef<Layout>();
   const background = createRef<Rect>();
 
@@ -21,44 +23,89 @@ export default makeScene2D(function* (view) {
   );
 
   view.add(
-    <Layout ref={layout} direction="column" gap={40} layout>
+    <Layout ref={layout} direction="column" gap={40} layout alignItems="center">
       <Txt
         ref={title}
         {...textStyles.h1}
       >
         Cellular Automata
       </Txt>
+      <Layout direction="column" gap={20} alignItems="center" position={[0, 100]}>
+        <Txt
+          ref={computational}
+          {...textStyles.h2}
+          fill={Colors.TEXT}
+          opacity={0}
+        >
+          Computational
+        </Txt>
+        <Txt
+          ref={abstract}
+          {...textStyles.h2}
+          fill={Colors.TEXT}
+          opacity={0}
+        >
+          Abstract
+        </Txt>
+        <Txt
+          ref={discrete}
+          {...textStyles.h2}
+          fill={Colors.TEXT}
+          opacity={0}
+        >
+          Discrete
+        </Txt>
+      </Layout>
       <Txt
-        ref={subtitle}
+        ref={emergent}
         {...textStyles.h2}
+        fill={Colors.TEXT}
+        opacity={0}
+        position={[0, 300]}
       >
-        Discrete - Abstract - Computational
-      </Txt>
-      <Txt
-        ref={definition}
-        {...textStyles.body}
-        maxWidth={1000}
-        textAlign="center"
-      >
-        A discrete computational model of a graph (usually a grid) that evolves over discrete time steps based on the state of the previous iteration.
+        Emergent Behaviour
       </Txt>
     </Layout>
   );
 
   // Initial state
   title().opacity(0);
-  subtitle().opacity(0);
-  definition().opacity(0);
+  layout().position.y(0);
 
-  // First slide: Title
+  // First slide: Title centered
   yield* title().opacity(1, 1);
   yield* beginSlide('title');
 
-  // Second slide: Subtitle
-  yield* subtitle().opacity(1, 1);
-  yield* beginSlide('subtitle');
+  // Second slide: Move title up and show all words
+  yield* all(
+    layout().position.y(-200, 1),
+    computational().opacity(1, 0.5),
+    abstract().opacity(1, 0.5),
+    discrete().opacity(1, 0.5)
+  );
+  yield* beginSlide('words');
 
-  // Third slide: Definition
-  yield* definition().opacity(1, 1);
-  yield* beginSlide('definition');
+  // Highlight words sequentially
+  yield* computational().fill('#ff6b6b', 0.5);
+  yield* beginSlide('highlight-computational');
+
+  yield* all(
+    computational().fill(Colors.TEXT, 0.5),
+    abstract().fill('#ff6b6b', 0.5)
+  );
+  yield* beginSlide('highlight-abstract');
+
+  yield* all(
+    abstract().fill(Colors.TEXT, 0.5),
+    discrete().fill('#ff6b6b', 0.5)
+  );
+  yield* beginSlide('highlight-discrete');
+
+  // Show emergent behavior and move words up
+  yield* all(
+    discrete().fill(Colors.TEXT, 0.5),
+    emergent().opacity(1, 0.5),
+    layout().position.y(-300, 0.5)
+  );
+  yield* beginSlide('emergent');
 }); 
